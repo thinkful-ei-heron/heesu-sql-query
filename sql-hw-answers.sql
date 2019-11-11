a5dece0fcfac32a3d4bcb9fc500f3ac1805b2591
Heesu Kang <hskang135@gmail.com>
2019-11-11 12:06:07 -0500

    done

--- sql-hw-answers.sql
+++ sql-hw-answers.sql
@@ -1,4 +1,4 @@
--- 1. How many people work in the Sales department?
+-- 1
 SELECT Count(e.emp_name), 
         d.dept_name
 FROM employee e 
@@ -7,7 +7,7 @@ ON e.department = d.id
 WHERE d.dept_name = 'Sales' 
 GROUP BY d.dept_name; 
 
--- 2. List the names of all employees assigned to the 'Plan Christmas party' project.
+-- 2
 SELECT e.emp_name,
         p.project_name
 FROM employee e
@@ -17,7 +17,7 @@ JOIN project p
 ON ep.project_id = p.id
 WHERE p.project_name = 'Plan christmas party';
 
--- 3. List the names of employees from the Warehouse department that are assigned to the 'Watch paint dry' project.
+-- 3
 SELECT e.emp_name
 FROM employee e
 JOIN department d
@@ -29,7 +29,7 @@ ON ep.project_id = p.id
 WHERE d.dept_name = 'Warehouse'
 AND p.project_name = 'Watch paint dry';
 
--- 4. Which projects are the Sales department employees assigned to?
+-- 4
 SELECT e.emp_name,
         p.project_name,
         d.dept_name
@@ -42,7 +42,7 @@ JOIN project p
 ON ep.project_id = p.id
 WHERE d.dept_name = 'Sales';
 
--- 5. List only the managers that are assigned to the 'Watch paint dry' project.
+-- 5
 SELECT e.emp_name AS Managers
 FROM department d
 JOIN employee e
